b829b8cc8154ba50a9f4cdb77b48d300ce77ddcb
cgomezlop95 <c.gomezlop95@gmail.com>
2023-11-20 20:22:37 +0100

    compose done

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from docker!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get("/", (req, res) => {
+  res.send("Hello from docker!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
