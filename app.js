0a06974394d8ad22f20f68091df7157a3b0f24c2
MatiKHO <matiasproint1@gmail.com>
2024-11-25 18:20:43 +0100

    Done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello from docker!");
 });
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
 });
